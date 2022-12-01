1c85b05f7dabc5659d96407fe4f99aac34db176f
Danil <59608495+Danil-Tolmachov@users.noreply.github.com>
2022-12-01 23:38:05 +0200

    Update clock_angle.py

--- clock_angle.py
+++ clock_angle.py
@@ -6,10 +6,7 @@ def clock_angle(hours: int, seconds: int) -> float:
     degree_hours = 360/24 * hours
     degree_seconds = 360/60 * seconds
 
-    if abs(degree_hours - degree_seconds) < 180:
-        return abs(degree_hours - degree_seconds)
-    else:
-        return abs(degree_seconds - degree_hours)
+    return abs(degree_hours - degree_seconds)
 
 
 assert clock_angle(12, 30) == 0
